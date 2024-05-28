b74de9ce59743ddc4b24c253c25793a08c7dcbde
Ty Byers <tyrone@wraphx.com>
2024-05-28 16:26:20 -0400

    Final Update

--- src/components/TexturePaths.jsx
+++ src/components/TexturePaths.jsx
@@ -1,83 +1,83 @@
 var TexturePaths = [
     {
         overlay: 1,
-        path: "textures/BaseMtl Base Color.png"
+        path: "textures/BaseMtl Base Color.002.png"
     },
     {
         overlay: 2,
-        path: "textures/Slaughter Base Color.png"
+        path: "textures/Slaughter Base Color.002.png"
     },
     {
         overlay: 3,
-        path: "textures/BatLady Base Color.png"
+        path: "textures/BatLady Base Color.002.png"
     },
     {
         overlay: 4,
-        path: "textures/BrakeLine Base Color.png"
+        path: "textures/BrakeLine Base Color.002.png"
     },
     {
         overlay: 5,
-        path: "textures/Danger Base Color.png"
+        path: "textures/Danger Base Color.002.png"
     },
     {
         overlay: 6,
-        path: "textures/Joker Base Color.png"
+        path: "textures/Joker Base Color.002.png"
     },
     {
         overlay: 7,
-        path: "textures/Madness Base Color.png"
+        path: "textures/Madness Base Color.002.png"
     },
     {
         overlay: 8,
-        path: "textures/Desert Base Color.png"
+        path: "textures/Desert Base Color.002.png"
     },
     {
         overlay: 9,
-        path: "textures/HotRods Base Color.png"
+        path: "textures/HotRods Base Color.002.png"
     },
     {
         overlay: 10,
-        path: "textures/Mayan Base Color.png"
+        path: "textures/Mayan Base Color.002.png"
     },
     {
         overlay: 11,
-        path: "textures/SeaBreeze Base Color.png"
+        path: "textures/SeaBreeze Base Color.002.png"
     },
     {
         overlay: 12,
-        path: "textures/ShapeShifter Base Color.png"
+        path: "textures/ShapeShifter Base Color.002.png"
     },
     {
         overlay: 13,
-        path: "textures/Silk Base Color.png"
+        path: "textures/Silk Base Color.002.png"
     },
     {
         overlay: 14,
-        path: "textures/SnakeSkin Base Color.png"
+        path: "textures/SnakeSkin Base Color.002.png"
     },
     {
         overlay: 15,
-        path: "textures/Flare Base Color.png"
+        path: "textures/Flare Base Color.002.png"
     },
     {
         overlay: 16,
-        path: "textures/Dominator Base Color.png"
+        path: "textures/Dominator Base Color.002.png"
     },
     {
         overlay: 17,
-        path: "textures/TopGun Base Color.png"
+        path: "textures/TopGun Base Color.002.png"
     },
     {
         overlay: 18,
-        path: "textures/Explosion Base Color.png"
+        path: "textures/Explosion Base Color.002.png"
     },
     {
         overlay: 19,
-        path: "textures/WildWest Base Color.png"
+        path: "textures/WildWest Base Color.002.png"
     },
     {
         overlay: 20,
-        path: "textures/Zombie Base Color.png"
+        path: "textures/Zombie Base Color.002.png"
     },
 
 ]
